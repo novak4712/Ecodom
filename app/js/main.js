5bb0652a3fbee49a89fce230a15401fccd6cab34
Sorokin Sergey <novak4803@gmail.com>
2018-02-08 23:13:40 +0300

    slider + profit

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,32 @@
 $(document).ready(function () {
-    $(function () {
-        $('.menu__icon').on('click', function () {
-            $(this).closest('.menu').toggleClass('menu_state_open');
-        });
+
+    $(".menu__icon").on("click", function () {
+        $(this).closest(".menu").toggleClass("menu_state_open");
     });
+
+    $(".form-section").on("click", ".prepend-icon", function () {
+        $(".prepend-icon").each(function () {
+            $(this).removeClass("select_input")
+        });
+        $(this).toggleClass("select_input");
+    });
+
+    $('#popup__toggle').click(function () {
+        var popup_id = $('#' + $(this).attr("rel"));
+        $(popup_id).show();
+        $('.overlay_form').show();
+    });
+    $('.overlay_form').click(function () {
+        $('.overlay_form, .modal-form').hide();
+    });
+
+
+
+        $('.flexslider').flexslider({
+            animation: "slide"
+        });
+
 });
+
+
+
